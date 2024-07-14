9a7543ce5345a9a0cf93de6148e83e3612cf1ce8
martin-lndbl-GTL <lblsolutions@outlook.de>
2024-07-14 13:34:13 +0200

    llama3 init

--- ask.sh
+++ ask.sh
@@ -1,6 +1,19 @@
-curl -s -X POST -H "Content-Type: application/json" -d '{
-  "system_message": "short answers",
-  "user_message": "Tell me a joke",
-  "max_tokens": 100
-}' http://172.19.0.50 | \
-  jq '.choices[0].text'
+#! /bin/sh -eux
+
+# text=$(curl -s -X POST -H "Content-Type: application/json" -d "{
+#   \"user_message\": \"${1}\",
+#   \"max_tokens\": 100
+# }" http://172.19.0.50 | \
+#   jq '.choices[0].text' | \
+#   sed 's/\\//g' | \
+#   jq -sRr @uri)
+
+# echo "$text"
+curl -H 'Content-Type: application/json' \
+     -d '{"contents":[
+            {"role": "user",
+              "parts":[{"text": "Give me five subcategories of jazz?"}]}]}' \
+     "https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${GEMINI}"
+
+
+# curl -s "http://172.19.0.51/api/tts?text=${text}&speaker_id=p231&style_wav=&language_id=" | aplay
---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Diane flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }@inputs:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          devShell = pkgs.mkShell {
+
+            nativeBuildInputs = with pkgs; [
+            ];
+
+            shellHook = ''
+              [ ! -f .env ] || export $(grep -v '^#' .env | xargs)
+            '';
+          };
+        }
+      );
+}
